63306e91734ca3e93660390fcfe9bda3bc086107
anaiva27 <nastia.iv.91@gmail.com>
2021-03-12 16:47:34 -0800

    saved book alert added, comments added

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,34 +6,31 @@ export default function Search() {
   //API Key HIDE IN PROCESS.ENV???
   const API_KEY = "AIzaSyDEqWOaZeHGCKfBxPOsQpRE5nwMILRZeIE";
 
-  //States
   const [bookSearch, setBookSearch] = useState([]);
   const [userInput, setuserInput] = useState([]);
 
-  // function setSearched(data) {
-  //   setBooksearch(data.items);
-  // }
-
+  // getting books from google api
   useEffect(() => {
     let URL = `https://www.googleapis.com/books/v1/volumes?q=${userInput}&key=${API_KEY}`;
     API.googleSearch(URL)
       .then((res) => {
-        console.log("*******", res);
         setBookSearch(res.data.items);
         //empty input
-        document.querySelector("#search").value = "";
+        document.getElementById("search").value = "";
       })
       .catch((err) => {
         console.log(err);
       });
   }, [userInput]);
 
+  // get book name from user's input
   function handleFormSubmit(event) {
     event.preventDefault();
-    const userInput = document.querySelector("#search").value.trim();
+    const userInput = document.getElementById("search").value.trim();
     setuserInput(userInput);
   }
 
+  // save a book into booksearch array
   function handleSaveBook(id) {
     for (let i = 0; i < bookSearch.length; i++) {
       if (id === bookSearch[i].id) {
@@ -47,13 +44,14 @@ export default function Search() {
           googleId: bookSearch[i].id,
         })
           .then(() => {
-            console.log("The book has been saved!");
+            alert("The book has been saved!");
           })
           .catch((err) => console.log(err));
       }
     }
   }
 
+  // display cards with search results
   const renderCards = () => {
     let mappedSearch = null;
     if (bookSearch.length > 0) {
@@ -84,15 +82,21 @@ export default function Search() {
       <div className="container container-fluid bg-dark text-white p-3 mb-2 ">
         <h3>Search For a Book</h3>
         <div className="input-group mb-3">
-  <input type="text"  id="search" className="form-control" aria-label="Enter the name of the book"/>
-  <div className="input-group-append">
-    <span  onClick={handleFormSubmit} className=" btn btn-outline-secondary btn-link">Submit</span>
-  </div>
-</div>
-        {/* <input type="text" id="search" />
-        <button type="submit" onClick={handleFormSubmit} className=" btn btn-outline-secondary ">
-          Submit
-        </button> */}
+          <input
+            type="text"
+            id="search"
+            className="form-control"
+            aria-label="Enter the name of the book"
+          />
+          <div className="input-group-append">
+            <span
+              onClick={handleFormSubmit}
+              className=" btn btn-outline-secondary btn-link"
+            >
+              Submit
+            </span>
+          </div>
+        </div>
       </div>
       <br />
       <div className="container container-fluid">
